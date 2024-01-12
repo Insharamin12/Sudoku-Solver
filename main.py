89ea5381efe6c7a7f296416e18f8f74949f09f95
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-12 19:58:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ class Board:
             else:
                 board_string += lower_lines
         return board_string
+        
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
 
 
 
