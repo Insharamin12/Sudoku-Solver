38ff19385fc683fc442a45a05f5b902a8c9852ef
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-12 19:22:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 class Board:
     def __init__(self, board):
         self.board = board
+        
+    def __str__(self):
+        upper_lines = f'\n╔═══{"╤═══"*2}{"╦═══"}{"╤═══"*2}{"╦═══"}{"╤═══"*2}╗\n'
+        middle_lines = f'╟───{"┼───"*2}{"╫───"}{"┼───"*2}{"╫───"}{"┼───"*2}╢\n'
+        lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
+        board_string = upper_lines
