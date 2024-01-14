6949e1f75a658c4ff2e790de38ed3323ef7fcbf9
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-14 21:25:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,8 @@ class Board:
     def valid_in_square(self, row, col, num):
         row_start = (row // 3) * 3
         col_start=(col // 3) * 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3):
+                if self.board[row_no][col_no] == num:
+                    return False
+        return True
