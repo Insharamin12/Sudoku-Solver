0abd0ae1bb3e97e4c4f1eb5846098e87081cd3d2
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-14 21:25:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,3 +42,8 @@ class Board:
 
     def valid_in_row(self, row, num):
         return num not in self.board[row]
+    def valid_in_col(self, col, num):
+        return all(
+            self.board[row][col] != num
+            for row in range(9)
+        )
